91a733c6a27f77c667c2e4d8d13c9714725adfea
fm0195 <fm0105@gmail.com>
2015-10-12 19:28:11 -0600

    Sin bugs.

--- Gyges.pl
+++ Gyges.pl
@@ -12,35 +12,37 @@ filaMoverAux(Turno, [(_,Fila,_) | RestoTablero], Actual, Res) :- Turno = 2, Fila
 filaMoverAux(Turno, [(_,Fila,_) | RestoTablero], Actual, Res) :- Turno = 2, Fila < Actual, filaMoverAux(Turno,RestoTablero,Fila,Res).
 %------------------------------------------------------------------------------
 %Mueve Ficha de Tablero y relaciona el resultado con Res.
-mover((Numero,Fila,Columna), Tablero, Sol):-delete(Tablero, (Ficha,Fila,Columna), NuevoTablero), %borrar la posicion antigua de la ficha
-											findall(Res,moverAux((Numero,Fila,Columna), Numero, NuevoTablero,[(Fila,Columna)], Res),List), %encontrar todas las soluciones y asociarlo a List
+mover(Turno, (Numero,Fila,Columna), Tablero, Sol):-delete(Tablero, (Ficha,Fila,Columna), NuevoTablero), %borrar la posicion antigua de la ficha
+											findall(Res,moverAux(Turno, (Numero,Fila,Columna), Numero, NuevoTablero,[(Fila,Columna)], Res),List), %encontrar todas las soluciones y asociarlo a List
 											list_to_set(List,Set), %Eliminar las casillas repetidas.
-											member(Sol,Set).		%Listar cada una de los posibles movimientos.
+											member(Sol,Set),  \+ tableroInvalido(Sol).		%Listar cada una de los posibles movimientos.
 
 %MOVER AUX: Mueve "Ficha" "Numero" posiciones en "Tablero" sin pasar por casillas ya visitadas en la lista "Mov", generando "NuevoTablero"  
-moverAux(Ficha, 0, Tablero, Mov, NuevoTablero) :- 	ocupado(Tablero, Ficha, Numero), %Si al final del movimiento se tiene una casilla ocupada
-													moverAux(Ficha,Numero,Tablero,Mov, NuevoTablero).%volver a mover la ficha la cantidad de espacios que representa la ficha en esa posicion.
-moverAux(Ficha, 0, Tablero, Mov, NuevoTablero) :- !, append(Tablero, [Ficha], NuevoTablero).%Agregar la nueva posicion de la ficha al tablero.
-moverAux((Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaFila is Fila +1, NuevaFila =< 5,%moverse una fila
+moverAux(Turno, Ficha, 0, Tablero, Mov, NuevoTablero) :- 	ocupado(Tablero, Ficha, Numero), %Si al final del movimiento se tiene una casilla ocupada
+													moverAux(Turno, Ficha,Numero,Tablero,Mov, NuevoTablero).%volver a mover la ficha la cantidad de espacios que representa la ficha en esa posicion.
+moverAux(Turno, Ficha, 0, Tablero, Mov, NuevoTablero) :- !, append(Tablero, [Ficha], NuevoTablero).%Agregar la nueva posicion de la ficha al tablero.
+moverAux(Turno, (Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaFila is Fila +1, NuevaFila =< 5,%moverse una fila
+															Turno = 2,
 															libre(Tablero, NuevaFila, Columna, NuevoNumero),%asegurarse que el espacio este libre
 															valido(NuevaFila, Columna, Mov),%asegurarse que el movimiento no sea hacia atras
 															append(Mov, [(NuevaFila, Columna)], NuevoMov),%agregar la casilla a la lista de movimientos
-															moverAux((Ficha, NuevaFila, Columna), NuevoNumero, Tablero, NuevoMov, Res).%recursivamente llamar hasta que se hayan movido todas las casillas necesarias
-moverAux((Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaFila is Fila -1, NuevaFila >= -1,
+															moverAux(Turno, (Ficha, NuevaFila, Columna), NuevoNumero, Tablero, NuevoMov, Res).%recursivamente llamar hasta que se hayan movido todas las casillas necesarias
+moverAux(Turno, (Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaFila is Fila -1, NuevaFila >= -1,
+															Turno = 1,
 															libre(Tablero, NuevaFila, Columna, NuevoNumero),
 															valido(NuevaFila, Columna, Mov),
 															append(Mov, [(NuevaFila, Columna)], NuevoMov),
-															moverAux((Ficha, NuevaFila, Columna), NuevoNumero, Tablero, NuevoMov, Res).
-moverAux((Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaColumna is Columna+1, NuevaColumna =< 5,
+															moverAux(Turno, (Ficha, NuevaFila, Columna), NuevoNumero, Tablero, NuevoMov, Res).
+moverAux(Turno, (Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaColumna is Columna+1, NuevaColumna =< 5,
 															libre(Tablero, Fila, NuevaColumna, NuevoNumero),
 															valido(Fila, NuevaColumna, Mov),
 															append(Mov, [(Fila, NuevaColumna)], NuevoMov),
-															moverAux((Ficha, Fila, NuevaColumna), NuevoNumero, Tablero, NuevoMov, Res).
-moverAux((Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaColumna is Columna-1, NuevaColumna >= 0,
+															moverAux(Turno, (Ficha, Fila, NuevaColumna), NuevoNumero, Tablero, NuevoMov, Res).
+moverAux(Turno, (Ficha, Fila, Columna), Numero, Tablero, Mov, Res):-	NuevoNumero is Numero-1, NuevaColumna is Columna-1, NuevaColumna >= 0,
 															libre(Tablero, Fila, NuevaColumna, NuevoNumero),
 															valido(Fila, NuevaColumna), Mov),
 															append(Mov, [(Fila, NuevaColumna)], NuevoMov),
-															moverAux((Ficha, Fila, NuevaColumna), NuevoNumero, Tablero, NuevoMov, Res).
+															moverAux(Turno, (Ficha, Fila, NuevaColumna), NuevoNumero, Tablero, NuevoMov, Res).
 %------------------------------------------------------------------------------
 %LIBRE: Revisa si la posicion "Columna" "Fila" esta libre 
 libre([],_,_,_).%si se llega a la lista vacia, la casilla esta vacía.
@@ -60,6 +62,10 @@ ocupado([(_,_, _) | RestoTablero], (Ficha, Fila, Columna),FichaActual) :-
 %VALIDO: Revisa que el movimiento que se vaya hacer no se haga a una casilla ya visitada.
 valido(Fila,Columna,Mov) :- member((Fila,Columna),Mov), !, fail.
 valido(_,_,_).
+
+tableroInvalido([]) :- !,fail.
+tableroInvalido([(_,Fila,Columna) | RestoFichas]) :- member((1,Fila,Columna),RestoFichas) ; member((2,Fila,Columna),RestoFichas) ; member((3,Fila,Columna),RestoFichas). 
+tableroInvalido([_| RestoFichas]) :- tableroInvalido(RestoFichas).
 %-----------------------------------------------------------------------------
 movimiento(Tablero, Turno, TurnoOponente, NuevoTablero, Solucion):-movimientoGanador(Tablero, Tablero, Turno, NuevoTablero,Solucion), !.
 movimiento(Tablero, Turno, TurnoOponente, NuevoTablero, Solucion):-movimientoNormal(Tablero, Tablero, Turno, TurnoOponente, NuevoTablero,Solucion), !.
@@ -76,17 +82,24 @@ movimientoGanador([_| RestoFichas], Tablero, Turno, NuevoTablero, Solucion) :-
 %MOVIMIENTO NORMAL: Realiza un movimiento normal si un ganador no existiera
 movimientoNormal([],_,_,_,_,_) :- !,fail.
 movimientoNormal([(NumFicha,Fila,Columna) | RestoFichas], Tablero, Turno, TurnoOponente, NuevoTablero, Solucion) :- 
-										%guitracer,trace,	
+										Turno=1,	
 										filaMover(Turno, Tablero, NumFila), 
-										Fila=NumFila, mover((NumFicha,Fila,Columna), Tablero, NuevoTablero), last(NuevoTablero,NuevaFicha), 
+										Fila=NumFila, mover(Turno, (NumFicha,Fila,Columna), Tablero, NuevoTablero), last(NuevoTablero,NuevaFicha), 
 										not(movimientoGanador(NuevoTablero,NuevoTablero,TurnoOponente,_,_)),
 										NuevaFicha=(_,FilaNueva,_), Fila >= FilaNueva,
+										Solucion = [(NumFicha,Fila,Columna),NuevaFicha], !.
+movimientoNormal([(NumFicha,Fila,Columna) | RestoFichas], Tablero, Turno, TurnoOponente, NuevoTablero, Solucion) :- 
+										Turno=2,	
+										filaMover(Turno, Tablero, NumFila), 
+										Fila=NumFila, mover(Turno, (NumFicha,Fila,Columna), Tablero, NuevoTablero), last(NuevoTablero,NuevaFicha), 
+										not(movimientoGanador(NuevoTablero,NuevoTablero,TurnoOponente,_,_)),
+										NuevaFicha=(_,FilaNueva,_), Fila =< FilaNueva,
 										Solucion = [(NumFicha,Fila,Columna),NuevaFicha], !.
 movimientoNormal([_| RestoFichas], Tablero, Turno,TurnoOponente, NuevoTablero, Solucion) :- 
 										movimientoNormal(RestoFichas,Tablero,Turno,TurnoOponente,NuevoTablero,Solucion).
 
 
-
-ganador(Ficha, Tablero, Turno, NuevoTablero) :- Turno = 1, mover(Ficha, Tablero, NuevoTablero), last(NuevoTablero,Sol), Sol=(_,Fila,_), Fila = -1.
-ganador(Ficha, Tablero, Turno, NuevoTablero) :- Turno = 2, mover(Ficha, Tablero, NuevoTablero), last(NuevoTablero,Sol), Sol=(_,Fila,_), Fila = 5.
+ganador(Ficha, Tablero, Turno, NuevoTablero) :- Turno = 1, mover(Turno, Ficha, Tablero, NuevoTablero), last(NuevoTablero,Sol), Sol=(_,Fila,_), Fila = -1.
+ganador(Ficha, Tablero, Turno, NuevoTablero) :- Turno = 2, mover(Turno, Ficha, Tablero, NuevoTablero), last(NuevoTablero,Sol), Sol=(_,Fila,_), Fila = 5.
 %[(2,0,0),(3,0,1),(2,0,2),(1,0,3),(1,0,4),(3,0,5) , (1,4,0),(3,4,1),(2,4,2),(2,4,3),(3,4,4),(1,4,5)]
+%mover(2,(1,0,3),[ (1, 0, 3), (1, 0, 4), (3, 0, 5), (1, 3, 0)],X).
